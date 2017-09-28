c11378442c43c60342bd145e8922b68306f148f2
viniciuspx <vinicius.gabriel.px@gmail.com>
2017-09-28 14:45:04 -0300

    scripts folder

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+function showHome() {
+  document.getElementById("div-text").innerHTML = "Home.";
+}
+
+function showMisc() {
+  document.getElementById("div-text").innerHTML = "Misc.";
+}
+
+function showMedia() {
+  document.getElementById("div-text").innerHTML = "Media.";
+}
+
+function showExtra() {
+  document.getElementById("div-text").innerHTML = "Extra.";
+}
+
